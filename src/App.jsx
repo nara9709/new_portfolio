5dd97c9d115da82364d9003fc939bb6fa42144f9
nara <worldkr19@gmail.com>
2023-01-29 18:08:25 -0500

    Move loading page from Home to Avatar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
-import { useState } from 'react';
-import styles from './App.module.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-import { ReactComponent as Logo } from '../src/svgs/logo.svg';
 
 function App() {
-  const [isReady, setReady] = useState(false);
-
-  document.onreadystatechange = () => {
-    if (document.readyState === 'complete') {
-      setReady(true);
-    }
-  };
-
   return (
     <>
-      {isReady === false && (
-        <div className={styles.loadingDiv}>
-          <Logo className={styles.logo} />
-        </div>
-      )}
-      {isReady && (
-        <>
-          <Header />
-          <Home />
-        </>
-      )}
+      <Header />
+      <Home />
     </>
   );
 }
--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+import styles from './Avatar.module.css';
 
 import ModelAni from './Model_ani';
+import { ReactComponent as Logo } from '../../svgs/logo.svg';
 
 function Avatar() {
+  const [isRendered, setRendered] = useState(false);
+
   return (
-    <div>
+    <div className={styles.avatarContainer}>
+      {!isRendered && (
+        <div className={styles.loadingDiv}>
+          <Logo className={styles.logo} />
+        </div>
+      )}
       <Canvas
         camera={{ position: [2, 0, 12.25], fov: 15 }}
         style={{
           background: 'transparent',
           height: '100vh',
           zIndex: 3,
+        }}
+        onCreated={({}) => {
+          setRendered(true);
         }}
       >
         <ambientLight intensity={1.25} />
