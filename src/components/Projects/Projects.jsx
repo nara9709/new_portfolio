3021977a7c2346d30b1d5d70aa528d4170fab7c9
nara <worldkr19@gmail.com>
2023-01-30 02:02:26 -0500

    Projects-e commerce website

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,84 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import styles from './Projects.module.css';
 
-function Projects(props) {
+function Projects() {
+  const [showSkin, setSkin] = useState(false);
+
   return (
-    <div>
-      <p>Projects</p>
-    </div>
+    <section className={styles.section}>
+      <h1>Projects</h1>
+      <div>
+        <ul>
+          <li
+            className={styles.skinbody}
+            onMouseEnter={() => setSkin(true)}
+            onMouseLeave={() => setSkin(false)}
+          >
+            <img
+              src="https://res.cloudinary.com/nara9709/image/upload/v1675057514/1_csgh5i.png"
+              alt="skinbody"
+            />
+            {showSkin && (
+              <div className={styles.skinModal}>
+                <span
+                  className={styles.closeIcon}
+                  onClick={() => {
+                    setSkin(false);
+                  }}
+                >
+                  <ion-icon name="close-outline"></ion-icon>
+                </span>
+                <div className={styles.desBox}>
+                  <h3>
+                    Ski N Body <br /> e-commerce web site
+                  </h3>
+                  <p>
+                    An E-commerce website where users can explore products,
+                    check reviews, save items to their cart, and write reviews✍🏻{' '}
+                    <br />
+                    Admin user can upload their item and control their products!
+                  </p>
+                  <div className={styles.stackBox}>
+                    <span className={styles.stack}>
+                      <ion-icon name="logo-firebase"></ion-icon> Firebase
+                    </span>
+                    <span className={styles.stack}>
+                      <ion-icon name="logo-react"></ion-icon> React
+                    </span>
+                    <span className={styles.stack}>
+                      <ion-icon name="logo-css3"></ion-icon> PostCSS
+                    </span>
+                  </div>
+                  <div>
+                    <button className={styles.button}>
+                      <a
+                        target="_blank"
+                        href="https://ski-n-body.netlify.app/"
+                        rel="noreferrer"
+                      >
+                        View Live
+                      </a>
+                    </button>
+                    <button className={styles.button}>
+                      <a
+                        target="_blank"
+                        href="https://github.com/nara9709/Smart-shop"
+                        rel="noreferrer"
+                      >
+                        Github
+                      </a>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
+          </li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </section>
   );
 }
 
