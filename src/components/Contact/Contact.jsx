ac78fe59bfc57d3ca84a9a2a19b8da9a51afa723
nara <worldkr19@gmail.com>
2023-01-31 14:27:32 -0500

    Contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,75 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import styles from './Contact.module.css';
+import emailjs from '@emailjs/browser';
+import { ReactComponent as Phone } from '../../svgs/phone.svg';
 
-function Contact(props) {
-  return <div>Contact</div>;
+function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    const formCurrent = form.current;
+
+    // Check user fill out all input
+    if (
+      !formCurrent.user_name.value ||
+      !formCurrent.user_email.value.value ||
+      !formCurrent.message.value
+    ) {
+      alert('Please fill out all📝  ');
+
+      return;
+    }
+
+    // Send email
+    emailjs
+      .sendForm(
+        'portfolio_mail',
+        'template_w4jc52k',
+        form.current,
+        'DrTfcAZlbqO1Il1Kl'
+      ) //
+      .then(
+        () => {
+          alert(
+            `your message has been sent successfully! I'll reply to you soon🥰`
+          );
+
+          // Clear all input
+          form.current.reset();
+        },
+        (err) => {
+          alert(err);
+        }
+      );
+  };
+
+  return (
+    <section className={styles.section}>
+      <h1>Contact</h1>
+      <div className={styles.mainContainer}>
+        <div className={styles.formContainer}>
+          <form ref={form}>
+            <span>
+              <label> Name</label>
+              <input type="text" name="user_name" />
+            </span>
+            <span>
+              <label> Email</label> <input type="email" name="user_email" />
+            </span>
+
+            <span>
+              <label>Message</label> <textarea name="message" />
+            </span>
+            <button onClick={sendEmail} className={styles.button}>
+              Send
+            </button>
+          </form>
+        </div>
+        <Phone className={styles.phone} />
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
